870a021243d87ecbbbc3759c77e2e8f4380ff702
Swayam-555 <radheshyam.panigrahi@gmail.com>
2021-05-08 21:16:07 +0530

    async to sync

--- sketch.js
+++ sketch.js
@@ -1,29 +1,53 @@
 var ball;
+var database;
+var position;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
+    console.log(database);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var databaseRef = database.ref("ball/position");
+    databaseRef.on("value",readPositions,errorCreated);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+
+
+function errorCreated(){
+    console.log("BigError occured")
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({
+
+        x: ball.x+x,
+        y: ball.y+y
+
+    })
+}
+
+function readPositions(data){
+    position = data.val();
+    console.log(position);
+    ball.x = position.x;
+    ball.y = position.y;
+
 }
